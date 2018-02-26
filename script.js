cd72fb892a23189077a7138efcd6a873e0f4c8f8
wilbert.waller <wilbert.waller@polarisalpha.com>
2018-02-26 14:50:52 -0700

    Add random button to generate random color inputs

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+// random elements
+var random = document.querySelector("button");
+var hex_values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
@@ -14,8 +18,25 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomizeColors() {
+  var color1_value = "#";
+  var color2_value = "#";
+  
+  for (var i = 0; i < 6; i++) {
+    color1_value += hex_values[Math.floor(Math.random() * 16)];
+    color2_value += hex_values[Math.floor(Math.random() * 16)];
+  }
+  
+  color1.value = color1_value;
+  color2.value = color2_value;
+  
+  setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
-setGradient();
+random.addEventListener("click", randomizeColors);
+
+randomizeColors();
